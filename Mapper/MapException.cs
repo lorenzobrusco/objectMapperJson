0df48cb5a3fe81320b03c210b71cbc8022f4e348
Lorenzo <lorenzo.brusco@integris.it>
2019-12-20 10:00:43 +0100

    Initial commit

---
+++ Mapper/MapException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mapper
+{
+    class MapException : Exception
+    {
+        public MapException(string message) : base(message) { }
+
+        public MapException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ Mapper/Mapper.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Mapper
+{
+    public static class Mapper
+    {
+        private static string SerializeToJson(object obj)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+                return json;
+            }
+            catch (Exception e)
+            {
+                throw new MapException("invalid conversion", e);
+            }
+        }
+
+        private static T DeserializeFromJson<T>(string jsonObj)
+        {
+            try
+            {
+                var result = JsonConvert.DeserializeObject<T>(jsonObj, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new MapException("invalid deserialize", e);
+            }
+        }
+
+        public static T Map<T>(object source)
+        {
+            return Mapper.DeserializeFromJson<T>(Mapper.SerializeToJson(source));
+        }
+
+
+
+    }
+}
